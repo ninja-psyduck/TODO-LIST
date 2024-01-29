adc7e586d6bbca92eb085a9d178d568bccb95f86
Keith <psyduckabc@outlook.com>
2024-01-29 17:00:11 +0800

    New feature 'Add Task'
    New feature: Add a task to the tasks list.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,32 @@
-import React from 'react';
+import React from "react";
 import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
-import Todo from './components/Todo';
+import Todo from "./components/Todo";
+import { useState } from "react";
+import { nanoid } from "nanoid";
 
 function App(props) {
-  const taskList = props.tasks.map((task) => (<Todo id={task.id} name={task.name} completed={task.completed} key={task.id} />));
+  const [tasks, setTasks] = useState(props.tasks); //hook
+
+  // 通过props传过来的对象数组生成一一个新的数组
+  const taskList = tasks?.map((task) => (
+    <Todo
+      id={task.id}
+      name={task.name}
+      completed={task.completed}
+      key={task.id}
+    />
+  ));
+  //addTask() 是一个callback prop
+  function addTask(name) {
+    const newTask = { id: `todo-${nanoid()}`, name, completed: false };
+    setTasks([...tasks, newTask]); // 展开运算符（spread operator），通过hook更新任务列表
+  }
+
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
-      <Form />
+      <Form addTask={addTask} />
       <div className="filters btn-group stack-exception">
         <FilterButton />
         <FilterButton />
@@ -18,7 +36,8 @@ function App(props) {
       <ul
         role="list"
         className="todo-list stack-large stack-exception"
-        aria-labelledby="list-heading">
+        aria-labelledby="list-heading"
+      >
         {taskList}
         {/* <Todo name="Eat" completed={true} id="todo-0"/>
        <Todo name="Sleep" completed={false} id="todo-1"/>
@@ -30,5 +49,4 @@ function App(props) {
   );
 }
 
-
-export default App
+export default App;
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,28 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Form(props) {
+  const [name, setName] = useState("");
 
+  {
+    /*
+    event.target.value 是指触发事件的元素的当前值
+    每当用户在输入框中输入或删除文本时，onChange 事件被触发，event.target.value 将包含当前输入框的值，并将其存储在组件的状态中。
+    */
+  }
+  function handleChange() {
+    setName(event.target.value);
+  }
 
-    return (
-        <form >
-            <h2 className="label-wrapper">
-                <label htmlFor="new-todo-input" className="label__lg">
-                    What needs to be done?
-                </label>
-            </h2>
-            <input
-                type="text"
-                id="new-todo-input"
-                className="input input__lg"
-                name="text"
-                autoComplete="off"
-            />
-            <button type="submit" className="btn btn__primary btn__lg">
-                Add
-            </button>
+  //当用户在子组件中点击按钮时，子组件可以调用父组件传递的回调函数，以便父组件可以更新其状态或执行其他操作。
+  function handleSubmit(event) {
+    event.preventDefault();
+    props.addTask(name); //当点击提交时，调用回调属性（函数），将新任务加到任务列表中
+    setName("");
+  }
 
-        </form>
-    );
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2 className="label-wrapper">
+        <label htmlFor="new-todo-input" className="label__lg">
+          What needs to be done?
+        </label>
+      </h2>
+      <input
+        type="text"
+        id="new-todo-input"
+        className="input input__lg"
+        name="text"
+        autoComplete="off"
+        value={name}
+        onChange={handleChange}
+      />
+      <button type="submit" className="btn btn__primary btn__lg">
+        Add
+      </button>
+    </form>
+  );
 }
 
 export default Form;
