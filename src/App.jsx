01e3abda1db484631338dac33b9572794972cdca
Keith <psyduckabc@outlook.com>
2024-02-14 16:29:23 +0800

    All finished
    All finished

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,76 @@ import FilterButton from "./components/FilterButton";
 import Todo from "./components/Todo";
 import { useState } from "react";
 import { nanoid } from "nanoid";
+// 页面从上到下依次是：表单（Form）、筛选器（FilterButton）、任务列表（对象数组）,包含多个Todo(对象)
+// 这个对象的值是用来筛选任务的函数
+const FILTER_MAP = {
+  All: () => true,
+  Active: (task) => !task.completed,
+  Completed: (task) => task.completed,
+};
+
+// 创建一个常量 FILTER_NAMES，其值是 FILTER_MAP 对象的所有键组成的数组
+const FILTER_NAMES = Object.keys(FILTER_MAP);
 
 function App(props) {
   //Hook
-  const [tasks, setTasks] = useState(props.tasks);
+  const [tasks, setTasks] = useState(props.tasks); //用来更新任务列表（对象数组）
+  const [filter, setFilter] = useState("All"); //筛选器的初始值是All
+
+  //callback prop for <Todo/>
+  function toggleTaskCompleted(id) {
+    const updatedTasks = tasks.map((task) => {
+      // if this task has the same ID as the edited task
+      if (id === task.id) {
+        // use object spread to make a new object
+        // 当传入的参数与对象数组里某个对象的id相同时，这个对象的completed属性取反并返回这个对象
+        //这里展开运算符用在对象上
+        return { ...task, completed: !task.completed };
+      }
+      return task; //如果不相同，返回原来的对象
+    });
+    setTasks(updatedTasks); //更新任务列表
+  }
+  //callback prop for <Todo/> 删除任务的函数（prop, 回调属性，回调函数）
+  function deleteTask(id) {
+    //通过filter()函数把符合的条件的对象保留下来，然后更新列表（对象数组）
+    const remainingTasks = tasks.filter((task) => id !== task.id);
+    setTasks(remainingTasks);
+  }
+
+  //callback prop for <Todo/> 编辑任务的函数（prop, 回调属性，回调函数）
+  function editTask(id, newName) {
+    const editedTaskList = tasks.map((task) => {
+      if (id === task.id) {
+        return { ...task, name: newName };
+        //不管是要更改对象的哪个属性，写在最后就行
+      }
+      return task;
+    });
+    setTasks(editedTaskList);
+  }
 
   // 通过props传过来的对象数组生成一一个新的数组
-  const taskList = tasks?.map((task) => (
-    <Todo
-      id={task.id}
-      name={task.name}
-      completed={task.completed}
-      key={task.id}
+  const taskList = tasks
+    .filter(FILTER_MAP[filter])
+    .map((task) => (
+      <Todo
+        id={task.id}
+        name={task.name}
+        completed={task.completed}
+        key={task.id}
+        toggleTaskCompleted={toggleTaskCompleted}
+        deleteTask={deleteTask}
+        editTask={editTask}
+      />
+    ));
+
+  const filterList = FILTER_NAMES.map((name) => (
+    <FilterButton
+      key={name}
+      name={name}
+      isPressed={name === filter}
+      setFilter={setFilter}
     />
   ));
 
@@ -33,10 +91,10 @@ function App(props) {
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
       <Form addTask={addTask} />
+      {/* 将回调属性传给子组件 */}
       <div className="filters btn-group stack-exception">
-        <FilterButton />
-        <FilterButton />
-        <FilterButton />
+        {filterList}
+        {/* 通过对象数组多次使用子组件，并把所有props传给子组件 */}
       </div>
       <h2 id="list-heading">{headingText}</h2>
       <ul
@@ -45,11 +103,7 @@ function App(props) {
         aria-labelledby="list-heading"
       >
         {taskList}
-        {/* <Todo name="Eat" completed={true} id="todo-0"/>
-       <Todo name="Sleep" completed={false} id="todo-1"/>
-       {/*将 true 视为 JavaScript 表达式，而不是字符串。*/}
-        {/* <Todo name="Repeat" completed={false} id="todo-2" /> */}
-        {/* 通过prop传入数据，类似于传参  */}
+        {/* 通过对象数组多次使用子组件，并把所有props传给子组件 */}
       </ul>
     </div>
   );
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,24 +1,86 @@
 import React from "react";
+import { useState } from "react";
 
+function Todo(props) {
+  const [isEditing, setEditing] = useState(false);
+  const [newName, setNewName] = useState("");
 
-export default function Todo(props){
-    return  (
-        <li className="todo stack-small">
-          <div className="c-cb">
-            <input id="{props.id}" type="checkbox" defaultChecked={props.completed} />
-            <label className="todo-label" htmlFor="{props.id}">
-              {props.name}
-            </label>
-          </div>
-          <div className="btn-group">
-            <button type="button" className="btn">
-              Edit <span className="visually-hidden">{props.name}</span>
-            </button>
-            <button type="button" className="btn btn__danger">
-              Delete <span className="visually-hidden">{props.name}</span>
-              {/*span标签实现无障碍，不可见，accessibility*/}
-            </button>
-          </div>
-        </li>
-      );
+  //用在<input>标签
+  function handleChange(e) {
+    setNewName(e.target.value);
+  }
+
+  //用在<form>标签
+  function handleSubmit(e) {
+    e.preventDefault();
+    props.editTask(props.id, newName); //通过回调函数更新任务列表（对象数组）
+    setNewName(""); //将状态设为默认值
+    setEditing(false); //设为false即关闭编辑面板
+  }
+
+  //编辑面板
+  const editingTemplate = (
+    <form className="stack-small" onSubmit={handleSubmit}>
+      <div className="form-group">
+        <label className="todo-label" htmlFor={props.id}>
+          New name for {props.name}
+        </label>
+        <input
+          id={props.id}
+          className="todo-text"
+          type="text"
+          value={newName}
+          onChange={handleChange}
+        />
+      </div>
+      <div className="btn-group">
+        <button
+          type="button"
+          className="btn todo-cancel"
+          onClick={() => setEditing(false)}
+        >
+          Cancel
+          <span className="visually-hidden">renaming {props.name}</span>
+        </button>
+        <button type="submit" className="btn btn__primary todo-edit">
+          Save
+          <span className="visually-hidden">new name for {props.name}</span>
+        </button>
+      </div>
+    </form>
+  );
+
+  //查看面板
+  const viewTemplate = (
+    <div className="stack-small">
+      <div className="c-cb">
+        <input
+          id={props.id}
+          type="checkbox"
+          defaultChecked={props.completed}
+          onChange={() => props.toggleTaskCompleted(props.id)}
+        />
+        <label className="todo-label" htmlFor={props.id}>
+          {props.name}
+        </label>
+      </div>
+      <div className="btn-group">
+        <button type="button" className="btn" onClick={() => setEditing(true)}>
+          Edit <span className="visually-hidden">{props.name}</span>
+        </button>
+        <button
+          type="button"
+          className="btn btn__danger"
+          onClick={() => props.deleteTask(props.id)}
+        >
+          Delete <span className="visually-hidden">{props.name}</span>
+        </button>
+      </div>
+    </div>
+  );
+  //条件渲染
+  //通过三元运算符判断哪个template会被浏览器渲染，点击Edit，isEditing变为为true，渲染editingTemplate，否则渲染viewTemplate
+  return <li className="todo">{isEditing ? editingTemplate : viewTemplate}</li>;
 }
+
+export default Todo;
